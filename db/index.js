478c6d2af54c2c2a308eb69fc9eed9feeaadbbca
ShaiqRaza <shaiqraza808y@gmail.com>
2024-12-23 17:46:35 +0500

    Do basic setup

---
+++ db/index.js
@@ -0,0 +1,11 @@
+import mongoose from 'mongoose'
+import dotenv from 'dotenv'
+dotenv.config()
+
+mongoose.connect(`${process.env.DB_URI}/practice`)
+.then(()=>{
+    console.log("dataBase connected Successfully");
+})
+.catch((err)=>{
+    console.log("dataBase connection failed: ", err.message);
+})
--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
-console.log('hello world')
+import express from 'express'
+const app = express();
+import dotenv from 'dotenv'
+dotenv.config()
+
+import '../db/index.js'//database connection
+
+app.get('/', (req, res)=>{
+    res.send("Welcome to my application!!")
+})
+
+const port = process.env.PORT || 3000;
+app.listen(port, ()=>{
+    console.log("App is running on port: ", port)
+})
