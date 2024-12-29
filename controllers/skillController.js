1f778df877a28b8bea6585ac502faf377e1244b3
ShaiqRaza <shaiqraza808y@gmail.com>
2024-12-29 14:22:37 +0500

    Make skill document controllers

--- controllers/skillController.js
+++ controllers/skillController.js
@@ -6,7 +6,61 @@ export const getAllSkills = async(req, res) =>{
         res.status(200).json(response);
     })
     .catch((err)=>{
-        console.log(err)
         res.status(500).send("Something error happened! Can't load All skills");
+    })
+}
+
+export const createSkill = async(req, res) =>{
+    const [title, description] = req.body;
+    const existingSkill = await skillModel.find({title});
+
+    if(existingSkill){
+        return res.status(500).send(`${title} skill already exists`);
+    }
+    const newSkill = await skillModel.create({
+        title,
+        description
+    })
+    .then((response)=>{
+        res.status(200).json(response);
+    })
+    .catch((err)=>{
+        res.status(500).send("Something error happened! Can't create new skill document");
+    })
+}
+
+export const updateSkill = async(req, res) =>{ 
+    const [title, description, id] = req.body;
+    const existingSkill = await skillModel.find({_id:id});
+
+    if(!existingSkill){
+        return res.status(500).send(`Something error happened! Target skill document not exists`);
+    }
+
+    existingSkill.description = description;
+    existingSkill.title = title;
+    await existingSkill.save()
+    .then((response)=>{
+        res.status(200).json(response);
+    })
+    .catch((err)=>{
+        res.status(500).send(`Something error happened! Can'ts update ${title} skill document`);
+    })
+}
+
+export const deleteSkill = async (req, res)=>{
+    const [title] = req.body;
+    const existingSkill = await skillModel.find({title});
+
+    if(!existingSkill){
+        return res.status(504).send("The target skill to delete doesn't even exits.")
+    }
+
+    await skillModel.deleteOne({title})
+    .then((response)=>{
+        return res.send(response)
+    })
+    .catch(()=>{
+        return res.status(400).send("Something error happened! Can't delete the skill document.")
     })
 }
