2ac84fa33fee1df0d605e538670a8cf611c38299
ShaiqRaza <shaiqraza808y@gmail.com>
2025-01-01 23:30:38 +0500

    Add multer configuration

---
+++ config/multerConfig.js
@@ -0,0 +1,21 @@
+import multer from 'multer';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        const uploadPath = path.join(__dirname, 'public', 'images');
+        cb(null, uploadPath);
+    },
+    filename: (req, file, cb) => {
+        const uniqueName = `${Date.now()}-${file.originalname}`;
+        cb(null, uniqueName);
+    },
+});
+
+const upload = multer({ storage: storage });
+
+export default upload;
