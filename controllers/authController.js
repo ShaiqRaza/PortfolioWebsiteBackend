f190fbe171b10b92990ef69f28ab0ff7b4269816
ShaiqRaza <shaiqraza808y@gmail.com>
2025-01-03 12:39:40 +0500

    Updae login function

--- controllers/authController.js
+++ controllers/authController.js
@@ -5,11 +5,13 @@ import bcrypt from 'bcrypt';
 
 export const loginAdmin = async(req, res) => {
     try{
+        const {password} = req.body;
+        if(!password)
+            return res.json({message: "Password is Required"})
         const about = await aboutModel.findOne();
         if(!about)
-            return res.status(404).json({ message: "About not found!" });
+            return res.status(404).json({ message: "Admin not found!" });
         const hash = about.password;
-        const {password} = req.body;
         bcrypt.compare(password, hash, function(err, result) {
             if(err)
                 return res.status(500).json({message: "Error occured in hash comparing", error: err.message})
@@ -17,6 +19,10 @@ export const loginAdmin = async(req, res) => {
                 res.cookie('admin', "success", {httpOnly: true});
                 return res.status(200).json({ message: "Login successful!" });
             }
+            else
+                res.json({
+                    message: "Password is Incorrect!"
+                })
         });
     }
     catch(err){
