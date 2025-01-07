7b2e34099f0b025faff693a56d4809f095a3f6df
ShaiqRaza <shaiqraza808y@gmail.com>
2025-01-07 13:17:25 +0500

    Add isLoggedin middleware to already made routes

--- routes/aboutRoutes.js
+++ routes/aboutRoutes.js
@@ -1,7 +1,8 @@
 import { createAbout } from "../controllers/aboutController.js";
+import { isLoggedin } from "../middlewares/isLoggedin.js";
 import express from 'express'
 const router = express.Router();
 
-router.post('/create', createAbout);
+router.post('/create', isLoggedin, createAbout);
 
 export default router;
