06155bc507db98600d33c7dc9f855234fc7b9984
ShaiqRaza <shaiqraza808y@gmail.com>
2025-01-07 14:02:43 +0500

    Set up route for get about

--- routes/aboutRoutes.js
+++ routes/aboutRoutes.js
@@ -1,9 +1,10 @@
-import { createAbout, updateAbout } from "../controllers/aboutController.js";
+import { createAbout, updateAbout, getAbout } from "../controllers/aboutController.js";
 import { isLoggedin } from "../middlewares/isLoggedin.js";
 import express from 'express'
 const router = express.Router();
 
 router.post('/create', isLoggedin, createAbout);
 router.post('/update', isLoggedin, updateAbout);
+router.get('/get', getAbout);
 
 export default router;
