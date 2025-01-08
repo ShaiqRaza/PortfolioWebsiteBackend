5005bc09aedd4aea1d534e9f413c5050fe953947
ShaiqRaza <shaiqraza808y@gmail.com>
2025-01-08 20:43:28 +0500

    Add updateAdmin controller

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -33,3 +33,50 @@ export const createAdmin = async (req, res) =>{
         })
     }
 }
+
+export const updateAdmin = async (req, res) =>{
+    try{
+        //for more security
+        const {email, password, currentPassword} = req.body;
+
+        if( !((email || password) && currentPassword) )
+            return res.status(400).json({message: "All fields are required"})
+        
+        const admin = await adminModel.findOne();
+        if(!admin)
+            return res.status(404).json({message: "There is no admin yet"})
+    
+        const response = await bcrypt.compare(currentPassword, admin.password);
+        
+        if(!response)
+            return res.status(401).json({message: "Password is incorrect"})
+
+        if(password){
+            // comparing new and prev password
+            const comparison = await bcrypt.compare(password, admin.password);
+
+            if(comparison && (!email || email==admin.email))
+                return res.status(409).json({message: "Nothing to change"})
+            
+            const salt = await bcrypt.genSalt(10);
+            const hash = await bcrypt.hash(password, salt);
+            admin.password = hash;
+           
+        }
+        //if password is not given then email must be given compulsory as I already checked it
+        else if(email==admin.email)
+            return res.status(409).json({message: "Nothing to change"})
+        else
+            admin.email = email;
+    
+        await admin.save();
+    
+        res.status(200).json({message: "Admin updated successfully"});
+    }
+    catch(err){
+        res.json({
+            message: "Something error occured in admin updation",
+            error: err.message
+        })
+    }
+}
--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -1,8 +1,9 @@
 import express from 'express'
 const router = express.Router();
 
-import {createAdmin} from '../controllers/adminController.js'
+import {createAdmin, updateAdmin} from '../controllers/adminController.js'
 
 router.post('/create', createAdmin)
+router.post('/update', updateAdmin)
 
 export default router;
