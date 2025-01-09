d6de43ca08b70f34fa8cc5d82667c9172527ac32
ShaiqRaza <shaiqraza808y@gmail.com>
2025-01-09 11:17:29 +0500

    add video delete upload functions

--- utils/uploadHandlers.js
+++ utils/uploadHandlers.js
@@ -33,3 +33,37 @@ export async function imageDelete(public_id) {
       throw new Error(`Image delete failed: ${err.message}`);
     }
 }
+
+export async function videoUpload(file) {
+  try{
+      if (!file) 
+        throw new Error("No Video provided to upload");
+
+      const b64 = Buffer.from(file.buffer).toString("base64");
+      let dataURI = "data:" + file.mimetype + ";base64," + b64;
+
+      const res = await cloudinary.uploader.upload(dataURI, {
+        resource_type: "video",
+      });
+      return res;
+  }
+  catch(err){
+    throw new Error(`Video upload failed: ${err.message}`);
+  }
+}
+
+export async function videoDelete(public_id) {
+  try{
+    if(!public_id)
+      throw new Error("no video id provided to delete");
+    const res = await cloudinary.uploader.destroy(public_id);
+
+    if (res.result === 'ok')
+      return res.result;
+    else
+        throw new Error('Failed to delete video');
+  }
+  catch(err){
+    throw new Error(`Video delete failed: ${err.message}`);
+  }
+}
