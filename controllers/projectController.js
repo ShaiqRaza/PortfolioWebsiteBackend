4ec163283d98cc762654f2ec271fd85eb9d78347
ShaiqRaza <shaiqraza808y@gmail.com>
2025-01-09 10:58:59 +0500

    add projectController file

---
+++ controllers/projectController.js
@@ -0,0 +1,18 @@
+import mongoose from 'mongoose';
+import projectModel from '../models/projectModel.js'
+
+export const getAllProjects = async(req, res) =>{
+
+}
+
+export const createProject = async(req, res) =>{
+    
+}
+
+export const updateProject = async(req, res) =>{ 
+    
+}
+
+export const deleteProject = async (req, res)=>{
+    
+}
