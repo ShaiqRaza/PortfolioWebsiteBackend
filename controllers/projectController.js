362728fcbe528704b2d8c487a6668211052032f3
ShaiqRaza <shaiqraza808y@gmail.com>
2025-01-09 11:20:09 +0500

    Create getAllProjects controller

--- controllers/projectController.js
+++ controllers/projectController.js
@@ -2,7 +2,16 @@ import mongoose from 'mongoose';
 import projectModel from '../models/projectModel.js'
 
 export const getAllProjects = async(req, res) =>{
-
+    try{
+        const projects = await projectModel.find();
+        res.status(200).send(projects);
+    }
+    catch(err){
+        res.status(500).json({            
+            message:"Something error happened! Can't load All projects",
+            error: err.message
+        });
+    }
 }
 
 export const createProject = async(req, res) =>{
