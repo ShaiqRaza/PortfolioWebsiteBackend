2be0b9eb058719c010b312e60fdfbb2bde597227
ShaiqRaza <shaiqraza808y@gmail.com>
2025-01-09 11:58:14 +0500

    add createProject controller

--- controllers/projectController.js
+++ controllers/projectController.js
@@ -1,5 +1,6 @@
 import mongoose from 'mongoose';
 import projectModel from '../models/projectModel.js'
+import { imageUpload, imageDelete, videoUpload, videoDelete } from '../utils/uploadHandlers.js';
 
 export const getAllProjects = async(req, res) =>{
     try{
@@ -19,10 +20,35 @@ export const createProject = async(req, res) =>{
         const {title, description} = req.body;
         if(!(title && description))
             return res.status(400).json({ message: "required fields are not given." });
-        
+
         const {images} = req.files;
         const {video} = req.file;
 
+        const uploadedImages = Promise.all(images.map(async (image) => {
+            const uploadedImage = await imageUpload(image);
+            return {
+                image: uploadedImage.secure_url,
+                image_id: uploadedImage.public_id
+            }
+        }));
+
+        const uploadedVideo = await videoUpload(video);
+
+        const newProject = await projectModel.create({
+            title,
+            description,
+            images: uploadedImages,
+            video: uploadedVideo.secure_url,
+            video_id: uploadedVideo.public_id
+        });
+
+        res.status(201).json(newProject);
+    }
+    catch(err){
+        res.status(500).json({            
+            message:"Something error happened! Cn't create new project",
+            error: err.message
+        });
     }
 }
 
