09b10e5a31724440c78194425db25c369b485c05
ShaiqRaza <shaiqraza808y@gmail.com>
2025-01-11 19:19:21 +0500

    setup addVideo controller

--- routes/projectRoutes.js
+++ routes/projectRoutes.js
@@ -1,7 +1,7 @@
 import express from 'express'
 const router = express.Router();
 
-import {getAllProjects, createProject, updateProject, deleteProject, addImage, deleteImage} from '../controllers/projectController.js'
+import {getAllProjects, createProject, updateProject, deleteProject, addImage, deleteImage, addVideo} from '../controllers/projectController.js'
 import upload from '../config/multerConfig.js'
 const uploadFields = upload.fields([
     { name: 'videos', maxCount: 1 },
@@ -13,5 +13,6 @@ router.post('/create',uploadFields, createProject);
 router.post('/update/:id',uploadFields, updateProject);
 router.post('/add-image/:id', upload.single('image'), addImage);
 router.post('/delete-image/:id', deleteImage);
+router.post('/add-video/:id', upload.single('video'), deleteImage);
 
 export default router;
