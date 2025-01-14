33a07ab9e7c5b5b6e7f90fd27d6f34031a2d53c9
ShaiqRaza <shaiqraza808y@gmail.com>
2025-01-14 11:55:36 +0500

    Make docs model

---
+++ models/docModel.js
@@ -0,0 +1,17 @@
+import mongoose from 'mongoose'
+
+const docSchema = mongoose.Schema({
+    image:{
+        type:String,
+        required: true
+    },
+    title:{
+        type:String,
+        required: true
+    },
+    //description is not compulsory
+    description: String
+});
+
+const docModel = mongoose.model('doc', docSchema);
+export default docModel;
