2ecf5166f1b8e78b148f742e5bcca9377ab1bd56
ShaiqRaza <shaiqraza808y@gmail.com>
2025-01-14 12:35:06 +0500

    Add updatedescription controller

--- controllers/docControllers.js
+++ controllers/docControllers.js
@@ -1,4 +1,5 @@
 import docModel from "../models/docModel.js";
+import mongoose from "mongoose";
 import { imageDelete, imageUpload } from "../utils/uploadHandlers.js";
 import fs from 'fs/promises'
 
@@ -64,7 +65,7 @@ export const updateTitle = async(req, res)=>{
 
         existingDoc.title = title;
         await existingDoc.save();
-        
+
         return res.json({
             data: existingDoc,
             message: "Title updated successfully."
@@ -73,6 +74,39 @@ export const updateTitle = async(req, res)=>{
     catch(err){
         res.status(500).json({            
             message:"Something error happened! Can't update title.",
+            error: err.message
+        });
+    }
+}
+
+export const updateDescription = async(req, res)=>{
+    try{
+        const {description} = req.body;
+        if(!description)
+            return res.status(400).json({message: "Nothing to update!"});
+
+        const id = req.params.id;
+        if(!id || !mongoose.Types.ObjectId.isValid(id))
+            return res.status(400).json({ message: "Document ID is not correct." });
+
+        const existingDoc = await docModel.findById(id);
+        if(!existingDoc)
+            return res.status(400).json({message: "Document, not found."})
+
+        if(description == existingDoc.description)
+            return res.status(400).json({message: "Nothing to update!"});
+
+        existingDoc.description = description;
+        await existingDoc.save();
+        
+        return res.json({
+            data: existingDoc,
+            message: "Description updated successfully."
+        });
+    }
+    catch(err){
+        res.status(500).json({            
+            message:"Something error happened! Can't update description.",
             error: err.message
         });
     }
--- routes/docRoutes.js
+++ routes/docRoutes.js
@@ -2,8 +2,10 @@ import express from "express";
 const router = express.Router();
 import upload from '../config/multerConfig.js'
 
-import { createDoc } from "../controllers/docControllers.js";
+import { createDoc, updateTitle, updateDescription } from "../controllers/docControllers.js";
 
 router.post('/create',upload.single('image'), createDoc )
+router.post('/update-title/:id', updateTitle )
+router.post('/update-description/:id', updateDescription )
 
 export default router;
