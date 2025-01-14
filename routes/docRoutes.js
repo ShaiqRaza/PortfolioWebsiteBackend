355bea0fa49a3942cace5f508a1981ab3a118494
ShaiqRaza <shaiqraza808y@gmail.com>
2025-01-14 13:14:04 +0500

    setup updateImage controller

--- routes/docRoutes.js
+++ routes/docRoutes.js
@@ -2,10 +2,11 @@ import express from "express";
 const router = express.Router();
 import upload from '../config/multerConfig.js'
 
-import { createDoc, updateTitle, updateDescription } from "../controllers/docControllers.js";
+import { createDoc, updateTitle, updateDescription, updateImage } from "../controllers/docControllers.js";
 
 router.post('/create',upload.single('image'), createDoc )
 router.post('/update-title/:id', updateTitle )
+router.post('/update-image/:id', upload.single('image'), updateImage )
 router.post('/update-description/:id', updateDescription )
 
 export default router;
