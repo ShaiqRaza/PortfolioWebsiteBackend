4f1e0f656c2495c82cf2e2f0e077d685c65274ab
ShaiqRaza <shaiqraza808y@gmail.com>
2025-01-15 14:29:41 +0500

    update doc controllers responses

--- controllers/docControllers.js
+++ controllers/docControllers.js
@@ -9,11 +9,11 @@ export const createDoc = async(req, res)=>{
     try{
         const image = req.file;
         if(!image)
-            return res.status(500).json({message: "All fields are required."});
+            return res.status(400).json({success: false, message: "All fields are required."});
 
         const {title, description=null} = req.body || {};
         if(!title)
-            return res.status(500).json({message: "All fields are required."});
+            return res.status(400).json({success: false, message: "All fields are required."});
 
         uploadedImage = await imageUpload(image.path);
 
@@ -26,6 +26,7 @@ export const createDoc = async(req, res)=>{
 
         await fs.unlink(image.path);
         res.status(200).json({
+            success: true,
             data: createdDoc,
             message: "Document created successfully"
         })
@@ -41,6 +42,7 @@ export const createDoc = async(req, res)=>{
             errorMessage = `${errorMessage} - CleanUpError: ${err.message}`
         }
         res.status(500).json({
+            success: false,
             message: "Something error occured at document creation",
             error: errorMessage
         })
@@ -51,29 +53,31 @@ export const updateTitle = async(req, res)=>{
     try{
         const {title} = req.body;
         if(!title)
-            return res.status(400).json({message: "Nothing to update!"});
+            return res.status(400).json({success: false, message: "Nothing to update!"});
 
         const id = req.params.id;
         if(!id || !mongoose.Types.ObjectId.isValid(id))
-            return res.status(400).json({ message: "Document ID is not correct." });
+            return res.status(400).json({success: false, message: "Document ID is not correct." });
 
         const existingDoc = await docModel.findById(id);
         if(!existingDoc)
-            return res.status(400).json({message: "Document, not found."})
+            return res.status(400).json({success: false, message: "Document not found."})
 
         if(title == existingDoc.title)
-            return res.status(400).json({message: "Nothing to update!"});
+            return res.status(400).json({success: false, message: "Nothing to update!"});
 
         existingDoc.title = title;
         await existingDoc.save();
 
-        return res.json({
+        return res.status(200).json({
+            success: true,
             data: existingDoc,
             message: "Title updated successfully."
         });
     }
     catch(err){
-        res.status(500).json({            
+        res.status(500).json({     
+            success: false,       
             message:"Something error happened! Can't update title.",
             error: err.message
         });
@@ -84,29 +88,31 @@ export const updateDescription = async(req, res)=>{
     try{
         const {description} = req.body;
         if(!description)
-            return res.status(400).json({message: "Nothing to update!"});
+            return res.status(400).json({success: false, message: "Nothing to update!"});
 
         const id = req.params.id;
         if(!id || !mongoose.Types.ObjectId.isValid(id))
-            return res.status(400).json({ message: "Document ID is not correct." });
+            return res.status(400).json({success: false, message: "Document ID is not correct." });
 
         const existingDoc = await docModel.findById(id);
         if(!existingDoc)
-            return res.status(400).json({message: "Document, not found."})
+            return res.status(400).json({success: false, message: "Document not found."})
 
         if(description == existingDoc.description)
-            return res.status(400).json({message: "Nothing to update!"});
+            return res.status(400).json({success: false, message: "Nothing to update!"});
 
         existingDoc.description = description;
         await existingDoc.save();
         
-        return res.json({
+        return res.status(200).json({
+            success: true,
             data: existingDoc,
             message: "Description updated successfully."
         });
     }
     catch(err){
-        res.status(500).json({            
+        res.status(500).json({   
+            success: false,         
             message:"Something error happened! Can't update description.",
             error: err.message
         });
@@ -118,15 +124,15 @@ export const updateImage = async(req, res)=>{
     try{
         const image = req.file;
         if(!image)
-            return res.status(500).json({message: "All fields are required."});
+            return res.status(400).json({success: false, message: "All fields are required."});
 
         const id = req.params.id;
         if(!id || !mongoose.Types.ObjectId.isValid(id))
-            return res.status(400).json({ message: "Document ID is not correct." });
+            return res.status(400).json({success: false, message: "Document ID is not correct." });
 
         const existingDoc = await docModel.findById(id);
         if(!existingDoc)
-            return res.status(400).json({message: "Document not found."});
+            return res.status(400).json({success: false, message: "Document not found."});
 
         uploadedImage = await imageUpload(image.path);
         
@@ -141,13 +147,15 @@ export const updateImage = async(req, res)=>{
             await imageDelete(existingDoc.image_id);
         }
         catch(err){
-            return res.status(500).json({
+            return res.status(200).json({
+                success: true,
                 data: existingDoc,
                 message: "Document created but image couldn't delete from server.",
             });
         }
 
         return res.status(200).json({
+            success: true,
             data: existingDoc,
             message: "Image updated successfully."
         });
@@ -163,6 +171,7 @@ export const updateImage = async(req, res)=>{
             errorMessage = `${errorMessage} - CleanUpError: ${err.message}`
         }
         res.status(500).json({
+            success: false,
             message: "Some error occured at image updation.",
             message: errorMessage
         })
