bf1e270db91fd3eb241d1a2e8404791e54ac0b01
ShaiqRaza <shaiqraza808y@gmail.com>
2025-01-15 14:39:17 +0500

    update auth controllers responses

--- controllers/authController.js
+++ controllers/authController.js
@@ -8,25 +8,26 @@ export const loginAdmin = async(req, res) => {
     try{
         const admin = await adminModel.findOne();
         if(!admin)
-            return res.status(404).json({ message: "There is no admin, you can't login" });
+            return res.status(400).json({success: false, message: "There is no admin, you can't login" });
         const {password, email} = req.body;
         if(!password || !email)
-            return res.json({message: "All fields are required"})
+            return res.status(400).json({success: false, message: "All fields are required"})
 
         if(email != admin.email )
-            return res.status(500).json({ message: "Incorrect email or password"})
+            return res.status(400).json({success: false, message: "Incorrect email or password"})
 
         const isMatch = await bcrypt.compare(password, admin.password);
         if (!isMatch)
-            return res.status(401).json({ message: "Incorrect email or password" });
+            return res.status(400).json({success: false, message: "Incorrect email or password" });
 
         const token = jwt.sign({email}, process.env.JWT_SECRET, {expiresIn: 60*60*24});
 
         res.cookie('admin', token, {httpOnly: true});
-        return res.status(200).json({ message: "Login successful!" });
+        return res.status(200).json({success: true, message: "Login successful!" });
     }
     catch(err){
         res.status(500).json({
+            success: false,
             message: "An error occures while login the admin",
             error: err.message
         })
@@ -36,10 +37,11 @@ export const loginAdmin = async(req, res) => {
 export const logoutAdmin = async(req, res) => {
     try{
         res.clearCookie('admin', {httpOnly:true});
-        return res.json({message: "Logged out successfully!"})
+        return res.status(200).json({success: true, message: "Logged out successfully!"})
     }
     catch(err){
         res.status(500).json({
+            success: false,
             message: "error occured at logging out",
             error: err.message
         })
