bff186bc42aee548ca1905ba44aafff2e017b2d9
ShaiqRaza <shaiqraza808y@gmail.com>
2025-01-15 14:43:16 +0500

    update admin controllers responses

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -6,15 +6,15 @@ import bcrypt from 'bcrypt'
 export const createAdmin = async (req, res) =>{
     try{
         if(process.env.NODE_ENV == "production")
-            return res.status(404).json({message:"You can't create Admin!"})
+            return res.status(400).json({success: false, message:"You can't create Admin!"})
         const availableAdmin = await adminModel.findOne();
         if(availableAdmin)
-            return res.status(500).json({message: "Admin is already created"})
+            return res.status(400).json({success: false, message: "Admin is already created"})
 
         const {email, password} = req.body;
 
         if(!(email && password))
-            return res.status(504).json({message: "All fields are required"})
+            return res.status(400).json({success: false, message: "All fields are required"})
 
         const salt = await bcrypt.genSalt(10);
         const hash = await bcrypt.hash(password, salt);
@@ -24,10 +24,15 @@ export const createAdmin = async (req, res) =>{
             email
         })
 
-        res.json({message: "Admin created successfully"})
+        res.status(200).json({
+            success: true,
+            message: "Admin created successfully",
+            data: admin
+        })
     }
     catch(err){
-        res.json({
+        res.status(500).json({
+            success: false,
             message: "Something error occured in admin creation",
             error: err.message
         })
@@ -40,23 +45,23 @@ export const updateAdmin = async (req, res) =>{
         const {email, password, currentPassword} = req.body;
 
         if( !((email || password) && currentPassword) )
-            return res.status(400).json({message: "All fields are required"})
+            return res.status(400).json({success: false, message: "All fields are required"})
         
         const admin = await adminModel.findOne();
         if(!admin)
-            return res.status(404).json({message: "There is no admin yet"})
+            return res.status(400).json({success: false, message: "There is no admin yet"})
     
         const response = await bcrypt.compare(currentPassword, admin.password);
         
         if(!response)
-            return res.status(401).json({message: "Password is incorrect"})
+            return res.status(400).json({success: false, message: "Password is incorrect"})
 
         if(password){
             // comparing new and prev password
             const comparison = await bcrypt.compare(password, admin.password);
 
             if(comparison && (!email || email==admin.email))
-                return res.status(409).json({message: "Nothing to change"})
+                return res.status(400).json({success: false, message: "Nothing to update"})
             
             const salt = await bcrypt.genSalt(10);
             const hash = await bcrypt.hash(password, salt);
@@ -65,16 +70,21 @@ export const updateAdmin = async (req, res) =>{
         }
         //if password is not given then email must be given compulsory as I already checked it
         else if(email==admin.email)
-            return res.status(409).json({message: "Nothing to change"})
+            return res.status(400).json({success: false, message: "Nothing to update"})
         else
             admin.email = email;
     
         await admin.save();
     
-        res.status(200).json({message: "Admin updated successfully"});
+        res.status(200).json({
+            success: true,
+            message: "Admin updated successfully",
+            data: admin
+        });
     }
     catch(err){
-        res.json({
+        res.status(500).json({
+            success: false,
             message: "Something error occured in admin updation",
             error: err.message
         })
