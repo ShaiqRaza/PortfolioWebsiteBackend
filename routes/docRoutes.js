1ca5ad9d07aaa1317cddd73d056ee3b52479ede9
ShaiqRaza <shaiqraza808y@gmail.com>
2025-01-17 11:57:11 +0500

    setup route for delete doc controller

--- routes/docRoutes.js
+++ routes/docRoutes.js
@@ -2,7 +2,7 @@ import express from "express";
 const router = express.Router();
 import upload from '../config/multerConfig.js'
 
-import { createDoc, updateTitle, updateDescription, updateImage, getAllDocs, removeDescription } from "../controllers/docControllers.js";
+import { createDoc, updateTitle, updateDescription, updateImage, getAllDocs, removeDescription, deleteDoc } from "../controllers/docControllers.js";
 
 router.get('/get-all', getAllDocs )
 router.post('/create',upload.single('image'), createDoc )
@@ -10,5 +10,6 @@ router.post('/update-title/:id', updateTitle )
 router.post('/update-image/:id', upload.single('image'), updateImage )
 router.post('/update-description/:id', updateDescription )
 router.post('/remove-description/:id', removeDescription )
+router.post('/delete/:id', deleteDoc )
 
 export default router;
