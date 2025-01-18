16ac60a67be976291c199c447fa733d1feebd0f1
ShaiqRaza <shaiqraza808y@gmail.com>
2025-01-18 13:39:27 +0500

    setup cors

--- src/index.js
+++ src/index.js
@@ -4,10 +4,12 @@ dotenv.config();
 import express from 'express';
 import cookieParser from 'cookie-parser';
 import '../db/index.js';
+import cors from 'cors';
 
 const app = express();
 
 // general Middleware
+app.use(cors());  
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
