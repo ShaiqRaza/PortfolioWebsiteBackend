8be76f1cc9f0d3d174e430c05b6ffa07d1db9d24
Shaiq Raza <shaiqraza808y@gmail.com>
2025-02-01 22:46:39 +0500

    Update authRoutes.js

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,9 +1,10 @@
-import { loginAdmin, logoutAdmin } from "../controllers/authController.js";
+import { loginAdmin, logoutAdmin, isLoggedIn } from "../controllers/authController.js";
 import { isLoggedin } from "../middlewares/isLoggedin.js";
 import express from 'express'
 const router = express.Router();
 
 router.post('/login', loginAdmin)
 router.post('/logout', isLoggedin, logoutAdmin)
+router.get('/', isLoggedIn)
 
 export default router;
