90fc93cf1cb667c6088052404a585de6e166127d
ShaiqRaza <shaiqraza808y@gmail.com>
2025-02-02 18:00:19 +0500

    update cookie options in login controller

--- controllers/authController.js
+++ controllers/authController.js
@@ -22,7 +22,7 @@ export const loginAdmin = async(req, res) => {
 
         const token = jwt.sign({email}, process.env.JWT_SECRET, {expiresIn: 60*60*24});
 
-        res.cookie('admin', token, {httpOnly: true, path: "/"});
+        res.cookie('admin', token, {httpOnly: true, path: "/", sameSite: "None", });
         return res.status(200).json({success: true, message: "Login successful!" });
     }
     catch(err){
